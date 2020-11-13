c63de3ecc654b0df7270033d842ff5c22dcc4c69
hefeng <503614875@qq.com>
2020-11-13 12:46:14 +0800

    paixu

---
+++ src/test/java/com/bright/learn/sort/SortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bright.learn.sort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
